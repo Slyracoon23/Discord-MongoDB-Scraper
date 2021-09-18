890e5723ed394c51901f9c514c254d0c20400bf3
slyRacoon23 <earl.potters@gmail.com>
2021-09-18 16:55:43 -0600

    refactored into classes

--- scrap-dicord-channel.py
+++ scrap-dicord-channel.py
@@ -1,35 +1,63 @@
-# import requests
-
-# channel_id = "848464764890775565"
-
-# headers = {'Authorization': 'NDI3NTQyODg1ODQ0NjQ3OTQ3.YS2nmQ.8C5lnmOXRC82_gPMGoZjGHkyTN0'}
-
-# url = "https://discord.com/api/v8/channels/{}/messages/search".format(channel_id)
-
-# response = requests.get( url, headers=headers)
-
-# assert response.status_code == 200
-# # print(response.text)
-
-# data = response.json()
-########################################################################333
-######################## DISCORD END ######################################
-########################################################################333
-
-############################################################################
-#################### MONGODB START #########################################
-########################################################################333
+import requests
 
 import pymongo
+from pymongo.errors import AutoReconnect, ConnectionFailure
 
-myclient =   pymongo.MongoClient("mongodb+srv://dbUser:AXfdXe5Sy2cX32q@discord-crypto.clfxu.mongodb.net", serverSelectionTimeoutMS=5000)
-print(myclient.server_info())
-mydb = myclient["Crypto-Currency-Discord"]
-mycol = mydb["General-Chat"]
+class DiscordScraper:
+    def __init__(self, channel_id, auth_token):
+        self.channel_id = channel_id
+        self.auth_token = auth_token
+        self.url = "https://discord.com/api/v8/channels/{}/messages/search".format(channel_id)
+        self.headers = {'Authorization': auth_token}
+
+    def get_messages(self):
+        '''
+        API Call only recieves ~25 of most recent messages
+        '''
+        response = requests.get( self.url, headers=self.headers)
+        assert response.status_code == 200
+        return response.json()
 
 
-mydict = { "name": "John", "address": "Highway 37" }
+class Mongo:
+    def __init__(self, client_uri="mongodb+srv://dbUser:AXfdXe5Sy2cX32q@discord-crypto.clfxu.mongodb.net", db="Crypto-Currency-Discord", col="General-Chat"):
+        self.client_uri = client_uri
+        self.myclient = self.createClient()
+        self.mydb = self.myclient[db]
+        self.mycol = self.mydb[col]
+
+    def createClient(self):
+        try:
+            myclient =  pymongo.MongoClient(self.client_uri , serverSelectionTimeoutMS=5000)
+        except (AutoReconnect, ConnectionFailure) as e:
+            print("Client Failed to Connect")
+            raise 
+
+        return myclient
+
+    def updateDB(self, data):
+        for post in data['messages']:
+            result = self.mycol.update_one(post[0],{"$set":post[0]},upsert=True)
+
+        return
+        
 
 
-for post in data['messages']:
-    result = mycol.update_one(post[0],{"$set":post[0]},upsert=True)
+
+
+if __name__ == '__main__':
+
+    # Variables
+    channel_id = "848464764890775565"
+    auth_token = 'NDI3NTQyODg1ODQ0NjQ3OTQ3.YS2nmQ.8C5lnmOXRC82_gPMGoZjGHkyTN0'
+
+
+    # instances
+    scraper = DiscordScraper(channel_id,auth_token)
+
+    data = scraper.get_messages()
+
+    mong = Mongo()
+
+    mong.updateDB(data)
+
